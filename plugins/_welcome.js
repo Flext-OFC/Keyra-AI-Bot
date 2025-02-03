eefc06380b965521d6131e018083dfe2dd7e2b1d
Flext-OFC <flextofc@gmail.com>
2025-02-03 17:31:21 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -21,8 +21,8 @@ export async function before(m, { conn, participants, groupMetadata }) {
     contextInfo: { forwardedNewsletterMessageInfo: { 
     newsletterJid: "120363307382381547@newsletter",
     serverMessageId: '', 
-    newsletterName: '─͟͞̟𝑴𝒆𝒈𝒖͜𝒎͜𝒊𝒏-𝑩͜𝒐𝒕-𝑴𝑫͟͞─' }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
-    "title": `  ͟͞ Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ ͟͞  `, 
+    newsletterName: '·.★·.·´¯`·.·★ 𝓝𝓮𝔂𝓻𝓪-𝓐𝓘-𝓑𝓸𝓽 ★·.·´¯`·.·★.·' }, forwardingScore: 9999999, isForwarded: true, mentionedJid: getMentionedJid(), "externalAdReply": { 
+    "title": `ıllıllı⭐🌟 Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ 🌟⭐ıllıllı`, 
     "body": `${userName}`, 
     "previewType": "PHOTO", 
     "thumbnailUrl": null,
