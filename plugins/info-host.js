ecb0152696b383ac798a718e6ffc579beb00de35
Flext-OFC <flextofc@gmail.com>
2025-02-04 18:16:14 -0300

    Actualizar info-host.js

--- plugins/info-host.js
+++ plugins/info-host.js
@@ -1,22 +1,22 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let txt = `🚩 *O L Y M P U S - H O S T* 
+let txt = `✅ *🌠 𝗔𝗞𝗜𝗥𝗔𝗫 - 𝗛𝗢𝗦𝗧 🛩️* 
 
 *¿Quieres un Host de calidad y con bajos precios?*
 Pues te presento a *OlympusHost*, un hosting de calidad con servidores dedicados y precios por debajo de 1USD, estos servidores están destinados a ofrecerte un Uptime 24/7 para que puedas alojar tus proyectos y qué estos funcionen de manera eficaz.
 
-🟢 \`\`\`Información del Host\`\`\`
+📈 \`\`\`Información del Host\`\`\`
 
-🔮 *Dashboard:* 
-• https://dash.olympus-host.xyz
+🛩️ *Home:* 
+• https://home.akirax.net
 
-🧃 *Panel:*
-• https://panel.olympus-host.xyz
+🌠 *Console:*
+• https://console.akirax.net
 
-💥 *Canal:*
-• https://whatsapp.com/channel/0029ValCkNT2ER6gHWFRQ71J
+💥✨ *Canal:*
+• httphttps://whatsapp.com/channel/0029Vb3Ocrs7IUYNQktqeE11
 
-⚜️ *Contacto (Mateo)*
-https://wa.me/593979133620
+📌 *Contacto (Matias-crypto)*
+https://wa.me/5491164123932
 
 > *Únete a está comunidad y disfruta de un servicio de calidad :D*` 
 await conn.sendMessage(m.chat, { text: txt,
@@ -26,11 +26,11 @@ isForwarded: false,
 "externalAdReply": {
 "showAdAttribution": true,
 "containsAutoReply": true,
-title: `🔵 O L Y M P U S - H O S T 🔵`,
+title: `🛩️ 𝑨𝑲𝑰𝑹𝑨𝑿 - 𝑯𝑶𝑺𝑻 💥`,
 body: `⚜️ Super Hosting 24/7 ⚜️`,
 "previewType": "PHOTO",
-thumbnailUrl: 'https://qu.ax/waCu.jpg', 
-sourceUrl: 'https://dash.olympus-host.xyz'}}},
+thumbnailUrl: 'https://files.catbox.moe/t59roe.jpg', 
+sourceUrl: 'https://console.akirax.net'}}},
 { quoted: fkontak})
 }
 handler.tags =['main'] 
