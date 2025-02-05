3286cfeb1b5f1988e299b5290c05a20af405f8f4
Flext-OFC <flextofc@gmail.com>
2025-02-04 21:00:06 -0300

    Actualizar owner-get.js

--- plugins/owner-get.js
+++ plugins/owner-get.js
@@ -22,5 +22,4 @@ const handler = async (m, {text}) => {
 handler.help = ['fetch', 'get'];
 handler.tags = ['owner'];
 handler.command = ['fetch', 'get'];
-handler.rowner = true;
 export default handler;
