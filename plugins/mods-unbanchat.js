f508756e85ab83d6c9346c513121dd3f79baf980
Flext-OFC <flextofc@gmail.com>
2025-02-06 21:04:41 -0300

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '📌 *¡ɴᴇʏʀᴀ-ᴀɪ-ʙᴏᴛ ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴀ ᴇɴ ᴇsᴛᴇ CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '📌 *¡ɴᴇʏʀᴀ-ᴀɪ-ʙᴏᴛ ɴᴏ ᴇsᴛᴀ ʙᴀɴᴇᴀᴅᴀ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
 chat.isBanned = false
 await conn.reply(m.chat, '✨ *¡ɴᴇʏʀᴀ-ᴀɪ-ʙᴏᴛ ʏᴀ ғᴜᴇ ᴅᴇsʙᴀɴᴇᴀᴅᴀ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ!*', m, fake)
 }
