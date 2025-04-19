82af18ad95c181786c59a2dc0cf3e6c768c8a0fd
Flext-OFC <flextofc@gmail.com>
2025-04-19 02:36:48 -0300

    Actualizar jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -70,7 +70,7 @@ return resultado;
 const message = users.map((v, index) => `• 「 ${index + 1} 」\n📎 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n👤 Usuario: ${v.user.name || 'Sub-Bot'}\n🕑 Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
 const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible por el momento, verifique mas tarde.` : message;
 const totalUsers = users.length;
-const responseMessage = `🌻 *LISTA DE SUBBOTS*\n\n⭐️ PUEDES PEDIR PERMISO PARA QUE TE DEJEN UNIR EL BOT A TÚ GRUPO\n\n\`\`\`CADA USUARIO SUB BOT USA FUNCIÓN COMO QUIERA, EL NÚMERO PRINCIPAL NO SE HACE RESPONSABLE DEL USO DE LA FUNCIÓN \`\`\`\n\nSUBBOT CONECTADO: ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
+const responseMessage = `🍧 *LISTA DE SUBBOTS NEYRA-BOT*\n\n✨ ★彡[ᴘᴜᴇᴅᴇꜱ ᴘᴇᴅɪʀ ᴘᴇʀᴍɪꜱᴏ ᴘᴀʀᴀ Qᴜᴇ ᴛᴇ ᴅᴇᴊᴇɴ ᴜɴɪʀ ᴇʟ ʙᴏᴛ ᴀ ᴛÚ ɢʀᴜᴘᴏ]彡★\n\n\`\`\`ᴄᴀᴅᴀ ᴜꜱᴜᴀʀɪᴏ ꜱᴜʙ ʙᴏᴛ ᴜꜱᴀ ꜰᴜɴᴄɪÓɴ ᴄᴏᴍᴏ Qᴜɪᴇʀᴀ, ᴇʟ ɴÚᴍᴇʀᴏ ᴘʀɪɴᴄɪᴘᴀʟ ɴᴏ ꜱᴇ ʜᴀᴄᴇ ʀᴇꜱᴘᴏɴꜱᴀʙʟᴇ ᴅᴇʟ ᴜꜱᴏ ᴅᴇ ʟᴀ ꜰᴜɴᴄɪÓɴ \`\`\`\n\nSUBBOT CONECTADO: ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
 await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})
 break   
 }}
